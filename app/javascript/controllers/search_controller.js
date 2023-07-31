ae76829a21b09509419cbf48a997697969c73115
elenachekhina <elenachekhina@yandex.ru>
2023-07-31 18:10:55 +0400

    Live search

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus";
+import { useDebounce } from 'stimulus-use'
+
+export default class extends Controller {
+    static debounces = ['search']
+    static targets = ['preview', 'search']
+
+    connect() {
+        useDebounce(this, { wait: 1000 })
+    }
+
+    search() {
+        let q = this.searchTarget.value
+        this.previewTarget.src = '/search?q=' + q + '&preview'
+    }
+
+    handleEnter(event) {
+        if (event.key === 'Enter') {
+            this.previewTarget.src = null
+        }
+    }
+
+    clear() {
+        this.searchTarget.value = null
+        this.previewTarget.src = null
+    }
+
+
+
+}
