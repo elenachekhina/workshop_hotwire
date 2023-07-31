d203547e69d7ad0b561c7a26cd990a599818e5df
elenachekhina <elenachekhina@yandex.ru>
2023-07-31 18:56:21 +0400

    Add 3 charter error handler to clear preview

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,23 +4,32 @@ import { useDebounce } from 'stimulus-use'
 export default class extends Controller {
     static debounces = ['search']
     static targets = ['preview', 'search']
+    enterPressed = false;
 
     connect() {
         useDebounce(this, { wait: 1000 })
     }
 
     search() {
+        if(this.enterPressed) {
+            this.enterPressed = false;
+            return;
+        }
         let q = this.searchTarget.value
         this.previewTarget.src = '/search?q=' + q + '&preview'
     }
 
     handleEnter(event) {
         if (event.key === 'Enter') {
+            this.enterPressed = true
             this.previewTarget.src = null
+        } else {
+            this.enterPressed = false
         }
     }
 
     clear() {
+        this.enterPressed = false
         this.searchTarget.value = null
         this.previewTarget.src = null
     }
